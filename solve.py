1623bce96d8601e91ea4e35a5950a90b54731167
Maxim Alexandrov <maxim_0101@bk.ru>
2024-11-25 23:30:30 +0300

    Улучшил читаемость питона

--- solve.py
+++ solve.py
@@ -11,21 +11,21 @@ def run_prog(code_folder):
     # Шаг 2: Создание таблицы для хранения результатов
     results_table = {}
     sizes = [1, 3, 20, 40]
-    ke_types = ['l', 'c']  # Типы данных или вычислений: 'l' и 'c'
+    ke_types = ['linear', 'cubic']  # Типы данных или вычислений: 'linear' и 'cubic'
 
     program_path = os.path.join(code_folder, 'build', 'main')
 
     for size in sizes:
         row = {}
         for ke_type in ke_types:
-            # Пропуск комбинаций 'l' с size=1 и 'c' с size=3
-            if (ke_type == 'l' and size == 1) or (ke_type == 'c' and size == 3):
+            # Пропуск комбинаций 'linear' с size=1 и 'cubic' с size=3
+            if (ke_type == 'linear' and size == 1) or (ke_type == 'cubic' and size == 3):
                 row[ke_type] = '-'  # Указать, что этот случай пропускается
                 continue
 
             # Запуск программы ./build/main с текущими параметрами
             result = subprocess.run(
-                [program_path, f'-{ke_type}', '-s', str(size)],
+                [program_path, f'--{ke_type}', '-s', str(size)],
                 capture_output=True, text=True
             )
 
@@ -60,5 +60,5 @@ if __name__ == "__main__":
     try:
         project_path = "."  # Корневая директория проекта
         results = run_prog(project_path)
-    except RuntimeError as e:
-        print(f"Ошибка: {e}")
+    except RuntimeError as error:
+        print(f"Ошибка: {error}")
