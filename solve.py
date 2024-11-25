8402462f12cd75c31afe8f49b4229b436e727026
Maxim Alexandrov <maxim_0101@bk.ru>
2024-11-26 00:14:03 +0300

    Убрал комментарии в pyhon

--- solve.py
+++ solve.py
@@ -3,27 +3,23 @@ import os
 
 
 def run_prog(code_folder):
-    # Шаг 1: Сборка программы с использованием make
     build_result = subprocess.run(['make', 'build'], cwd=code_folder)
     if build_result.returncode != 0:
         raise RuntimeError("Сборка завершилась с ошибкой.")
 
-    # Шаг 2: Создание таблицы для хранения результатов
     results_table = {}
     sizes = [1, 3, 20, 40]
-    ke_types = ['linear', 'cubic']  # Типы данных или вычислений: 'linear' и 'cubic'
+    ke_types = ['linear', 'cubic']
 
     program_path = os.path.join(code_folder, 'build', 'main')
 
     for size in sizes:
         row = {}
         for ke_type in ke_types:
-            # Пропуск комбинаций 'linear' с size=1 и 'cubic' с size=3
             if (ke_type == 'linear' and size == 1) or (ke_type == 'cubic' and size == 3):
-                row[ke_type] = '-'  # Указать, что этот случай пропускается
+                row[ke_type] = '-'
                 continue
 
-            # Запуск программы ./build/main с текущими параметрами
             result = subprocess.run(
                 [program_path, f'--{ke_type}', '-s', str(size)],
                 capture_output=True, text=True
@@ -31,24 +27,19 @@ def run_prog(code_folder):
 
             print(result.stdout)
 
-            # Проверка на успешное выполнение
             if result.returncode != 0:
                 raise RuntimeError(f"Ошибка выполнения {program_path} с параметрами -{ke_type} -s {size}")
 
-    # Шаг 3: Организация выходных файлов
     img_folder = os.path.join(code_folder, 'images')
     res_folder = os.path.join(code_folder, 'results')
 
-    # Создание папок для изображений и результатов, если они ещё не существуют
     os.makedirs(img_folder, exist_ok=True)
     os.makedirs(res_folder, exist_ok=True)
 
-    # Перемещение всех изображений (*.png) в папку images
     for file in os.listdir(code_folder):
         if file.endswith('.png'):
             os.rename(os.path.join(code_folder, file), os.path.join(img_folder, file))
 
-    # Перемещение всех текстовых файлов (*.txt) в папку results
     for file in os.listdir(code_folder):
         if file.endswith('.txt') and file != "CMakeLists.txt":
             os.rename(os.path.join(code_folder, file), os.path.join(res_folder, file))
@@ -58,7 +49,7 @@ def run_prog(code_folder):
 
 if __name__ == "__main__":
     try:
-        project_path = "."  # Корневая директория проекта
+        project_path = "."
         results = run_prog(project_path)
     except RuntimeError as error:
         print(f"Ошибка: {error}")
